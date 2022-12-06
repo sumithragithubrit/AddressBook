5eef803efa31f03f222c95606611dbee9baea7df
sumi <sumipavib1302@gmail.com>
2022-12-06 20:28:42 +0530

    details

--- src/addressbook/welcome.java
+++ src/addressbook/welcome.java
@@ -4,6 +4,22 @@ public class welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
+		 details obj = new details();
+	        obj.setName("sumithra");
+	        obj.setSurname("Buddareddy");
+	        obj.setAddress("Madanapali,Andrapradesh");
+	        obj.setCity("Chittor");
+	        obj.setState("Andrapradesh");
+	        obj.setContactNumber("6300036180");
+	        obj.setEmail("sumipavib1302@gmail.com");
+
+	        System.out.println("Name: " + obj.getName());
+	        System.out.println("Surname: " + obj.getSurname());
+	        System.out.println("Address: " + obj.getAddress());
+	        System.out.println("City: " + obj.getCity());
+	        System.out.println("State: " + obj.getState());
+	        System.out.println("ContactNumber: " + obj.getContactNumber());
+	        System.out.println("Email: " + obj.getEmail());
 
 	}
 
